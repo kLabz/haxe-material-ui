576e0304f948060747245f3fea82e3d80624e62c
k <k@klabz.org>
2018-08-28 14:00:23 +0200

    Added typings for ListItemText

--- src/mui/core/ListItemText.hx
+++ src/mui/core/ListItemText.hx
@@ -2,6 +2,20 @@ package mui.core;
 
 import react.ReactComponent;
 
+import mui.core.Typography.TypographyProps;
+import mui.core.list.ListItemTextClassKey;
+
+typedef ListItemTextProps = {
+	> StandardProps<ListItemTextClassKey>,
+
+	@:optional var disableTypography:Bool;
+	@:optional var inset:Bool;
+	@:optional var primary:ReactFragment;
+	@:optional var primaryTypographyProps:TypographyProps;
+	@:optional var secondary:ReactFragment;
+	@:optional var secondaryTypographyProps:TypographyProps;
+}
+
 @:jsRequire('@material-ui/core', 'ListItemText')
-extern class ListItemText extends ReactComponent {}
+extern class ListItemText extends ReactComponentOfProps<ListItemTextProps> {}
 
---
+++ src/mui/core/list/ListItemTextClassKey.hx
@@ -0,0 +1,11 @@
+package mui.core.list;
+
+enum abstract ListItemTextClassKey(String) to String {
+	var Root = "root";
+
+	var Inset = "inset";
+	var Dense = "dense";
+	var Primary = "primary";
+	var Secondary = "secondary";
+	var TextDense = "textDense";
+}
