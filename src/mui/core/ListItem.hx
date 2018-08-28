cad3b378301270c3f9bbdebc03b75a2b287c1193
k <k@klabz.org>
2018-08-28 13:54:00 +0200

    Added typings for ListItem

--- src/mui/core/ListItem.hx
+++ src/mui/core/ListItem.hx
@@ -1,7 +1,25 @@
 package mui.core;
 
 import react.ReactComponent;
+import react.ReactNode;
+
+import mui.core.ButtonBase;
+import mui.core.list.ListItemClassKey;
+
+typedef ListItemProps = {
+	> StandardProps<ListItemClassKey>,
+	> ButtonBaseBaseProps,
+
+	@:optional var button:Bool;
+	@:optional var component:ReactNode;
+	@:optional var ContainerComponent:ReactNode;
+	@:optional var ContainerProps:Dynamic;
+	@:optional var dense:Bool;
+	@:optional var disabled:Bool;
+	@:optional var disabledGutters:Bool;
+	@:optional var divider:Bool;
+}
 
 @:jsRequire('@material-ui/core', 'ListItem')
-extern class ListItem extends ReactComponent {}
+extern class ListItem extends ReactComponentOfProps<ListItemProps> {}
 
---
+++ src/mui/core/list/ListItemClassKey.hx
@@ -0,0 +1,15 @@
+package mui.core.list;
+
+enum abstract ListItemClassKey(String) to String {
+	var Root = "root";
+
+	var Container = "container";
+	var FocusVisible = "focusVisible";
+	var Default = "default";
+	var Dense = "dense";
+	var Disabled = "disabled";
+	var Divider = "divider";
+	var Gutters = "gutters";
+	var Button = "button";
+	var SecondaryAction = "secondaryAction";
+}
