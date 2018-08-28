b574cbe260d978cb1f8a8095633c3b52a4987b7c
k <k@klabz.org>
2018-08-28 11:49:30 +0200

    Added typings for CardHeader

--- src/mui/core/CardHeader.hx
+++ src/mui/core/CardHeader.hx
@@ -1,6 +1,23 @@
 package mui.core;
 
 import react.ReactComponent;
+import react.ReactNode;
+
+import mui.core.Typography.TypographyProps;
+import mui.core.card.CardHeaderClassKey;
+
+typedef CardHeaderProps = {
+	> StandardProps<CardHeaderClassKey>,
+
+	@:optional var action:ReactFragment;
+	@:optional var avatar:ReactFragment;
+	@:optional var component:ReactNode;
+	@:optional var disableTypography:Bool;
+	@:optional var subheader:ReactFragment;
+	@:optional var subheaderTypographyProps:TypographyProps;
+	@:optional var title:ReactFragment;
+	@:optional var titleTypographyProps:TypographyProps;
+}
 
 @:jsRequire('@material-ui/core', 'CardHeader')
-extern class CardHeader extends ReactComponent {}
+extern class CardHeader extends ReactComponentOfProps<CardHeaderProps> {}
---
+++ src/mui/core/card/CardHeaderClassKey.hx
@@ -0,0 +1,11 @@
+package mui.core.card;
+
+enum abstract CardHeaderClassKey(String) to String {
+	var Root = "root";
+
+	var Avatar = "avatar";
+	var Action = "action";
+	var Content = "content";
+	var Title = "title";
+	var SubHeader = "subheader";
+}
