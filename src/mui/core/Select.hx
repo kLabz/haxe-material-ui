08957885e87763dea366efec317c1a946df42aad
k <k@klabz.org>
2018-08-28 16:18:26 +0200

    Added typings for Select

--- src/mui/core/Select.hx
+++ src/mui/core/Select.hx
@@ -1,6 +1,34 @@
 package mui.core;
 
+import js.html.HtmlElement;
 import react.ReactComponent;
+import react.ReactNode;
+import react.types.DOMAttributes.HandlerOrVoid;
+import react.types.EventHandler;
+
+import mui.core.input.InputValue;
+import mui.core.input.SelectClassKey;
+import mui.core.Input.InputBaseProps;
+
+typedef SelectProps = {
+	> StandardProps<SelectClassKey>,
+	> InputBaseProps,
+
+	@:optional var autoWidth:Bool;
+	@:optional var displayEmpty:Bool;
+	@:optional var IconComponent:ReactNode;
+	@:optional var input:ReactSingleFragment;
+	@:optional var inputProps:Dynamic;
+	@:optional var MenuProps:Dynamic;
+	@:optional var multiple:Bool;
+	@:optional var native:Bool;
+	@:optional var onChange:HandlerOrVoid<FormEventHandler<HtmlElement>>;
+	@:optional var onClose:HandlerOrVoid<FormEventHandler<HtmlElement>>;
+	@:optional var open:Bool;
+	@:optional var renderValue:Any->ReactFragment;
+	@:optional var SelectDisplayProps:Dynamic;
+	@:optional var value:InputValue;
+}
 
 @:jsRequire('@material-ui/core', 'Select')
-extern class Select extends ReactComponent {}
+extern class Select extends ReactComponentOfProps<SelectProps> {}
---
+++ src/mui/core/input/SelectClassKey.hx
@@ -0,0 +1,10 @@
+package mui.core.input;
+
+enum abstract SelectClassKey(String) to String {
+	var Root = "root";
+
+	var Select = "select";
+	var SelectMenu = "selectMenu";
+	var Disabled = "disabled";
+	var Icon = "icon";
+}
