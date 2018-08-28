1430d6a4ee2ada3ea61330480360cdf87fa67294
k <k@klabz.org>
2018-08-28 14:38:43 +0200

    Added typings for Tab

--- src/mui/core/Tab.hx
+++ src/mui/core/Tab.hx
@@ -1,7 +1,22 @@
 package mui.core;
 
 import react.ReactComponent;
+import react.types.Noise;
+
+import mui.core.ButtonBase.ButtonBaseBaseProps;
+import mui.core.tab.TabClassKey;
+
+typedef TabProps = {
+	> StandardProps<TabClassKey>,
+	> ButtonBaseBaseProps,
+
+	@:optional var children:Noise;
+	@:optional var disabled:Bool;
+	@:optional var icon:ReactFragment;
+	@:optional var label:ReactFragment;
+	@:optional var value:Any;
+}
 
 @:jsRequire('@material-ui/core', 'Tab')
-extern class Tab extends ReactComponent {}
+extern class Tab extends ReactComponentOfProps<TabProps> {}
 
---
+++ src/mui/core/tab/TabClassKey.hx
@@ -0,0 +1,17 @@
+package mui.core.tab;
+
+enum abstract TabClassKey(String) to String {
+	var Root = "root";
+
+	var LabelIcon = "labelIcon";
+	var TextColorInherit = "textColorInherit";
+	var TextColorPrimary = "textColorPrimary";
+	var TextColorSecondary = "textColorSecondary";
+	var Selected = "selected";
+	var Disabled = "disabled";
+	var FullWidth = "fullWidth";
+	var Wrapper = "wrapper";
+	var LabelContainer = "labelContainer";
+	var Label = "label";
+	var LabelWrapped = "labelWrapped";
+}
