d356df1e7c14a087df8559b1aa467752f503a864
k <k@klabz.org>
2018-08-28 12:42:59 +0200

    Added FormLabel with typings

---
+++ src/mui/core/FormLabel.hx
@@ -0,0 +1,21 @@
+package mui.core;
+
+import react.ReactComponent;
+import react.ReactNode;
+
+import mui.core.form.FormLabelClassKey;
+
+typedef FormLabelProps = {
+	> StandardProps<FormLabelClassKey>,
+
+	@:optional var component:ReactNode;
+	@:optional var disabled:Bool;
+	@:optional var error:Bool;
+	@:optional var filled:Bool;
+	@:optional var focused:Bool;
+	@:optional var required:Bool;
+}
+
+@:jsxRequire('@material-ui/core', 'FormLabel')
+extern class FormLabel extends ReactComponentOfProps<FormLabelProps> {}
+
---
+++ src/mui/core/form/FormLabelClassKey.hx
@@ -0,0 +1,12 @@
+package mui.core.form;
+
+enum abstract FormLabelClassKey(String) to String {
+	var Root = "root";
+
+	var Focused = "focused";
+	var Disabled = "disabled";
+	var Error = "error";
+	var Filled = "filled";
+	var Required = "required";
+	var Asterisk = "asterisk";
+}
