65d05fcdd80d4d7d49a6078282ce6c26d978ccb0
k <k@klabz.org>
2018-08-28 11:58:32 +0200

    Added typings for CardContent

--- src/mui/core/CardContent.hx
+++ src/mui/core/CardContent.hx
@@ -1,6 +1,15 @@
 package mui.core;
 
 import react.ReactComponent;
+import react.ReactNode;
+
+import mui.core.card.CardContentClassKey;
+
+typedef CardContentProps = {
+	> StandardProps<CardContentClassKey>,
+
+	@:optional var component:ReactNode;
+}
 
 @:jsRequire('@material-ui/core', 'CardContent')
-extern class CardContent extends ReactComponent {}
+extern class CardContent extends ReactComponentOfProps<CardContentProps> {}
---
+++ src/mui/core/card/CardContentClassKey.hx
@@ -0,0 +1,5 @@
+package mui.core.card;
+
+enum abstract CardContentClassKey(String) to String {
+	var Root = "root";
+}
