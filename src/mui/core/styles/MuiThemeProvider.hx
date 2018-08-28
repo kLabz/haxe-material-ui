84fc34c61e8dbf6aa93b1cc375c2871aa5b93ef5
k <k@klabz.org>
2018-08-28 14:58:12 +0200

    Added typings for MuiThemeProvider

--- src/mui/core/styles/MuiThemeProvider.hx
+++ src/mui/core/styles/MuiThemeProvider.hx
@@ -1,7 +1,17 @@
 package mui.core.styles;
 
+import haxe.Constraints.Function;
+import haxe.extern.EitherType;
+
 import react.ReactComponent;
 
+typedef MuiThemeProviderProps = {
+	var children:ReactSingleFragment;
+	var theme:EitherType<Function, Dynamic>;
+	@:optional var disableStyles:Bool;
+	@:optional var sheetsManager:Dynamic;
+}
+
 @:jsRequire('@material-ui/core/styles', 'MuiThemeProvider')
-extern class MuiThemeProvider extends ReactComponent {} // TODO: props
+extern class MuiThemeProvider extends ReactComponentOfProps<MuiThemeProviderProps> {}
 
