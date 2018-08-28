cddd678ce55995319a6539275213e7cd5b203edb
k <k@klabz.org>
2018-08-28 13:46:43 +0200

    Added typings for List

--- src/mui/core/List.hx
+++ src/mui/core/List.hx
@@ -1,7 +1,19 @@
 package mui.core;
 
 import react.ReactComponent;
+import react.ReactNode;
+
+import mui.core.list.ListClassKey;
+
+typedef ListProps = {
+	> StandardProps<ListClassKey>,
+
+	@:optional var component:ReactNode;
+	@:optional var dense:Bool;
+	@:optional var disablePadding:Bool;
+	@:optional var subheader:ReactFragment;
+}
 
 @:jsRequire('@material-ui/core', 'List')
-extern class List extends ReactComponent {}
+extern class List extends ReactComponentOfProps<ListProps> {}
 
---
+++ src/mui/core/list/ListClassKey.hx
@@ -0,0 +1,9 @@
+package mui.core.list;
+
+enum abstract ListClassKey(String) to String {
+	var Root = "root";
+
+	var Padding = "padding";
+	var Dense = "dense";
+	var SubHeader = "subheader";
+}
