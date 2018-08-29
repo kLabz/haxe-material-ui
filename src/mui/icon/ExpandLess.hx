6c5b52c475544fbebab3c2442d951eab8cc6218b
k <k@klabz.org>
2018-08-29 13:37:01 +0200

    Added ExpandLess and ExpandMore icons

---
+++ src/mui/icon/ExpandLess.hx
@@ -0,0 +1,7 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'ExpandLess')
+extern class ExpandLess extends ReactComponentOfProps<IconProps> {}
+
---
+++ src/mui/icon/ExpandMore.hx
@@ -0,0 +1,7 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'ExpandMore')
+extern class ExpandMore extends ReactComponentOfProps<IconProps> {}
+
