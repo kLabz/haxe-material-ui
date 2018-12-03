75eff4015ad8e07d407ffbcf6df5d80d1b84a964
k <k@klabz.org>
2018-12-03 18:50:55 +0100

    Added Avatar

---
+++ src/mui/core/Avatar.hx
@@ -0,0 +1,23 @@
+package mui.core;
+
+import react.ReactComponent;
+
+import mui.core.avatar.AvatarClassKey;
+
+typedef AvatarProps = {
+	> StandardProps<AvatarClassKey>,
+
+	@:optional var alt:String;
+	@:optional var children:ReactFragment;
+	@:optional var component:ReactType;
+	@:optional var imgProps:Dynamic; // TODO: use html attributes when available
+	@:optional var sizes:String;
+	@:optional var src:String;
+	@:optional var srcSet:String;
+}
+
+@:acceptsMoreProps
+@:jsRequire('@material-ui/core', 'Avatar')
+extern class Avatar extends ReactComponentOfProps<AvatarProps> {}
+
+
---
+++ src/mui/core/avatar/AvatarClassKey.hx
@@ -0,0 +1,7 @@
+package mui.core.avatar;
+
+@:enum abstract AvatarClassKey(String) to String {
+	var Root = "root";
+	var ColorDefault = "colorDefault";
+	var Img = "img";
+}
