18d29de0d70cf660a319525af645f938c610e646
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SettingsSystemDaydream icon

---
+++ src/mui/icon/SettingsSystemDaydream.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SettingsSystemDaydream')
+extern class SettingsSystemDaydream extends ReactComponentOfProps<IconProps> {}
