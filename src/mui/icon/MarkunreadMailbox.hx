41cf4d66f5b6b2c2db0b8ce16fb8d7948d5d9730
k <k@klabz.org>
2018-12-14 09:26:29 +0100

    Added MarkunreadMailbox icon

---
+++ src/mui/icon/MarkunreadMailbox.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'MarkunreadMailbox')
+extern class MarkunreadMailbox extends ReactComponentOfProps<IconProps> {}
