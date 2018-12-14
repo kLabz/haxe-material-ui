0c2f971fe1578b56aa1c49296b2fd1fedbeca1ad
k <k@klabz.org>
2018-12-14 09:26:33 +0100

    Added Rotate90DegreesCcw icon

---
+++ src/mui/icon/Rotate90DegreesCcw.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'Rotate90DegreesCcw')
+extern class Rotate90DegreesCcw extends ReactComponentOfProps<IconProps> {}
