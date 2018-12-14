fd241b045f0e8398c333735a35aa5c1c102b1db2
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SettingsInputSvideo icon

---
+++ src/mui/icon/SettingsInputSvideo.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SettingsInputSvideo')
+extern class SettingsInputSvideo extends ReactComponentOfProps<IconProps> {}
