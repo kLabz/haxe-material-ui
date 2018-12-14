8afc62923de5ed4bbb3a02ed133cc95a744c4e9b
k <k@klabz.org>
2018-12-14 09:26:31 +0100

    Added PhoneBluetoothSpeaker icon

---
+++ src/mui/icon/PhoneBluetoothSpeaker.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'PhoneBluetoothSpeaker')
+extern class PhoneBluetoothSpeaker extends ReactComponentOfProps<IconProps> {}
