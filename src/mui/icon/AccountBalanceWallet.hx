e9d9c491201084ba47fc134287d791be0635cb6f
k <k@klabz.org>
2018-12-14 09:26:18 +0100

    Added AccountBalanceWallet icon

---
+++ src/mui/icon/AccountBalanceWallet.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'AccountBalanceWallet')
+extern class AccountBalanceWallet extends ReactComponentOfProps<IconProps> {}
