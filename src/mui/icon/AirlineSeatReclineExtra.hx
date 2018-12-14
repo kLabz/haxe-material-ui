0a353cdaaa4bf07572d412bdba7b4e5c8e081d70
k <k@klabz.org>
2018-12-14 09:26:18 +0100

    Added AirlineSeatReclineExtra icon

---
+++ src/mui/icon/AirlineSeatReclineExtra.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'AirlineSeatReclineExtra')
+extern class AirlineSeatReclineExtra extends ReactComponentOfProps<IconProps> {}
