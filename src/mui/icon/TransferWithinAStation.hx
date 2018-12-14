513e96aab7048fccd3c9dc8ca785d72b40be57a2
k <k@klabz.org>
2018-12-14 09:26:37 +0100

    Added TransferWithinAStation icon

---
+++ src/mui/icon/TransferWithinAStation.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'TransferWithinAStation')
+extern class TransferWithinAStation extends ReactComponentOfProps<IconProps> {}
