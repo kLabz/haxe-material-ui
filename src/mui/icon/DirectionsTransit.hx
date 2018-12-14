4ae19eb140cfce48b0979c21754275a4e7eb74eb
k <k@klabz.org>
2018-12-14 09:26:22 +0100

    Added DirectionsTransit icon

---
+++ src/mui/icon/DirectionsTransit.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'DirectionsTransit')
+extern class DirectionsTransit extends ReactComponentOfProps<IconProps> {}
