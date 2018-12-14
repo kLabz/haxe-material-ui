cb4d7399f1e3126b6c03fbf3091c68cb57105be0
k <k@klabz.org>
2018-12-14 09:26:29 +0100

    Added MobileScreenShare icon

---
+++ src/mui/icon/MobileScreenShare.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'MobileScreenShare')
+extern class MobileScreenShare extends ReactComponentOfProps<IconProps> {}
