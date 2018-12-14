a52fb7c0910e5ead92f3f6c761f7e3fb87d63269
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SettingsApplications icon

---
+++ src/mui/icon/SettingsApplications.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SettingsApplications')
+extern class SettingsApplications extends ReactComponentOfProps<IconProps> {}
