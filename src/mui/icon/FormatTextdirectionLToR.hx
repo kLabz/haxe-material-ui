262f6d57431b50d29d205888a28918ebf0d22758
k <k@klabz.org>
2018-12-14 09:26:25 +0100

    Added FormatTextdirectionLToR icon

---
+++ src/mui/icon/FormatTextdirectionLToR.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'FormatTextdirectionLToR')
+extern class FormatTextdirectionLToR extends ReactComponentOfProps<IconProps> {}
