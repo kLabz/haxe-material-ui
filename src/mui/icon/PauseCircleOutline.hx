cac430eb2942334f467f1cb3f972e8ab6df0fe4a
k <k@klabz.org>
2018-12-14 09:26:31 +0100

    Added PauseCircleOutline icon

---
+++ src/mui/icon/PauseCircleOutline.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'PauseCircleOutline')
+extern class PauseCircleOutline extends ReactComponentOfProps<IconProps> {}
