9a86bc540c7ab0b4be6e09cfd5382b73469da68d
k <k@klabz.org>
2018-12-14 09:26:25 +0100

    Added FormatListBulleted icon

---
+++ src/mui/icon/FormatListBulleted.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'FormatListBulleted')
+extern class FormatListBulleted extends ReactComponentOfProps<IconProps> {}
