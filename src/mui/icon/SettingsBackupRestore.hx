a92f002874ca98a949f36abb9917fadab9bef338
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SettingsBackupRestore icon

---
+++ src/mui/icon/SettingsBackupRestore.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SettingsBackupRestore')
+extern class SettingsBackupRestore extends ReactComponentOfProps<IconProps> {}
