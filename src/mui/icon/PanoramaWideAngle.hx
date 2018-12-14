c993a7d6ce16a2e553c6a29eeaa66414466b5875
k <k@klabz.org>
2018-12-14 09:26:31 +0100

    Added PanoramaWideAngle icon

---
+++ src/mui/icon/PanoramaWideAngle.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'PanoramaWideAngle')
+extern class PanoramaWideAngle extends ReactComponentOfProps<IconProps> {}
