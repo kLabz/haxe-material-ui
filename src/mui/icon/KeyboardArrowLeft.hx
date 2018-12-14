64c8320af6ee415d028240a4e67d5fc043f5eae0
k <k@klabz.org>
2018-12-14 09:26:27 +0100

    Added KeyboardArrowLeft icon

---
+++ src/mui/icon/KeyboardArrowLeft.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'KeyboardArrowLeft')
+extern class KeyboardArrowLeft extends ReactComponentOfProps<IconProps> {}
