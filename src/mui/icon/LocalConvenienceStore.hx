0a42ff2aa85a2d1adbe08881f79532d008b9f27a
k <k@klabz.org>
2018-12-14 09:26:28 +0100

    Added LocalConvenienceStore icon

---
+++ src/mui/icon/LocalConvenienceStore.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'LocalConvenienceStore')
+extern class LocalConvenienceStore extends ReactComponentOfProps<IconProps> {}
