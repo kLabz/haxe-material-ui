6b383e36b456108fb9a1e517555b391504eb102a
k <k@klabz.org>
2018-12-14 09:26:35 +0100

    Added SignalCellularConnectedNoInternet1Bar icon

---
+++ src/mui/icon/SignalCellularConnectedNoInternet1Bar.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SignalCellularConnectedNoInternet1Bar')
+extern class SignalCellularConnectedNoInternet1Bar extends ReactComponentOfProps<IconProps> {}
