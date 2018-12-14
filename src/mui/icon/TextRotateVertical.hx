d5a0cb9889a4876accf7337b2d3479a29a7bfdaa
k <k@klabz.org>
2018-12-14 09:26:37 +0100

    Added TextRotateVertical icon

---
+++ src/mui/icon/TextRotateVertical.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'TextRotateVertical')
+extern class TextRotateVertical extends ReactComponentOfProps<IconProps> {}
