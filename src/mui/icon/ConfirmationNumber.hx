77017648585bc755907bb4833e451f9abb04b10a
k <k@klabz.org>
2018-12-14 09:26:21 +0100

    Added ConfirmationNumber icon

---
+++ src/mui/icon/ConfirmationNumber.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'ConfirmationNumber')
+extern class ConfirmationNumber extends ReactComponentOfProps<IconProps> {}
