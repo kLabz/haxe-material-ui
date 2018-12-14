1c1fea8ff5ef3cbdad48b4876f1d42b82f503bca
k <k@klabz.org>
2018-12-14 09:26:33 +0100

    Added RemoveShoppingCart icon

---
+++ src/mui/icon/RemoveShoppingCart.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'RemoveShoppingCart')
+extern class RemoveShoppingCart extends ReactComponentOfProps<IconProps> {}
