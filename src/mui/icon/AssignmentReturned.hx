b332423d234de031c01e972eb9078f640762d9e2
k <k@klabz.org>
2018-12-14 09:26:19 +0100

    Added AssignmentReturned icon

---
+++ src/mui/icon/AssignmentReturned.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'AssignmentReturned')
+extern class AssignmentReturned extends ReactComponentOfProps<IconProps> {}
