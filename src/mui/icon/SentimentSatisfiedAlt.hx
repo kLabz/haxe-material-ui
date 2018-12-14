dd85575eb13e2b0d007e9cd396fd4eb1217ae08c
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SentimentSatisfiedAlt icon

---
+++ src/mui/icon/SentimentSatisfiedAlt.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SentimentSatisfiedAlt')
+extern class SentimentSatisfiedAlt extends ReactComponentOfProps<IconProps> {}
