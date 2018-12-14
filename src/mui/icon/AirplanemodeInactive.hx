08969640b7aeb7262ce56ee09530129c441080dd
k <k@klabz.org>
2018-12-14 09:26:18 +0100

    Added AirplanemodeInactive icon

---
+++ src/mui/icon/AirplanemodeInactive.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'AirplanemodeInactive')
+extern class AirplanemodeInactive extends ReactComponentOfProps<IconProps> {}
