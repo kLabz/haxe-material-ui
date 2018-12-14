90238f0a7dc12fc538a9e9be8bcdf90633c98ef4
k <k@klabz.org>
2018-12-14 09:26:31 +0100

    Added PanoramaHorizontal icon

---
+++ src/mui/icon/PanoramaHorizontal.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'PanoramaHorizontal')
+extern class PanoramaHorizontal extends ReactComponentOfProps<IconProps> {}
