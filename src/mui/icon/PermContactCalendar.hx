3fea9a11f63290118ab2ce35b3ab6ba62328b4b8
k <k@klabz.org>
2018-12-14 09:26:31 +0100

    Added PermContactCalendar icon

---
+++ src/mui/icon/PermContactCalendar.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'PermContactCalendar')
+extern class PermContactCalendar extends ReactComponentOfProps<IconProps> {}
