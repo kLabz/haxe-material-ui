9df54083ce608ac2b4bfde73a7810921b4fc3692
k <k@klabz.org>
2018-12-14 09:26:28 +0100

    Added LocalGroceryStore icon

---
+++ src/mui/icon/LocalGroceryStore.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'LocalGroceryStore')
+extern class LocalGroceryStore extends ReactComponentOfProps<IconProps> {}
