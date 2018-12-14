871f951c785ea676adde5a470b407a4ced380042
k <k@klabz.org>
2018-12-14 09:26:21 +0100

    Added CancelPresentation icon

---
+++ src/mui/icon/CancelPresentation.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'CancelPresentation')
+extern class CancelPresentation extends ReactComponentOfProps<IconProps> {}
