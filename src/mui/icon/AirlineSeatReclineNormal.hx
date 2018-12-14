229cff25766ed2fbb5cea08252603f7997abfa6e
k <k@klabz.org>
2018-12-14 09:26:18 +0100

    Added AirlineSeatReclineNormal icon

---
+++ src/mui/icon/AirlineSeatReclineNormal.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'AirlineSeatReclineNormal')
+extern class AirlineSeatReclineNormal extends ReactComponentOfProps<IconProps> {}
