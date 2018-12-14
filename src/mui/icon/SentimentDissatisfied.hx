8f6b5e77cb1658d2b6d2fc06152dc937e6b67b09
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SentimentDissatisfied icon

---
+++ src/mui/icon/SentimentDissatisfied.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SentimentDissatisfied')
+extern class SentimentDissatisfied extends ReactComponentOfProps<IconProps> {}
