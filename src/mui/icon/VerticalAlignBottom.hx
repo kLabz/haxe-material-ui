5c10bdebd5e39da32c74be08470595c16bb6d2c8
k <k@klabz.org>
2018-12-14 09:26:38 +0100

    Added VerticalAlignBottom icon

---
+++ src/mui/icon/VerticalAlignBottom.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'VerticalAlignBottom')
+extern class VerticalAlignBottom extends ReactComponentOfProps<IconProps> {}
