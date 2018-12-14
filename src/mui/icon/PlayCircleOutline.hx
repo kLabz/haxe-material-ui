bdef16ba598a21173d0d45fd09fcb32d83807d67
k <k@klabz.org>
2018-12-14 09:26:32 +0100

    Added PlayCircleOutline icon

---
+++ src/mui/icon/PlayCircleOutline.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'PlayCircleOutline')
+extern class PlayCircleOutline extends ReactComponentOfProps<IconProps> {}
