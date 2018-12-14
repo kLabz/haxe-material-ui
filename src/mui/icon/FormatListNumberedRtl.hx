ab4506695bd71f9dfda8f6b8b5b5c6adf2fcc5a7
k <k@klabz.org>
2018-12-14 09:26:25 +0100

    Added FormatListNumberedRtl icon

---
+++ src/mui/icon/FormatListNumberedRtl.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'FormatListNumberedRtl')
+extern class FormatListNumberedRtl extends ReactComponentOfProps<IconProps> {}
