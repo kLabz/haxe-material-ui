d64e45c2b038a5ebe470b6325ab3eba73153c0c1
k <k@klabz.org>
2018-12-14 09:26:32 +0100

    Added PlayCircleFilledWhite icon

---
+++ src/mui/icon/PlayCircleFilledWhite.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'PlayCircleFilledWhite')
+extern class PlayCircleFilledWhite extends ReactComponentOfProps<IconProps> {}
