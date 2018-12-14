394e0835f28a8ca61a023e9161cc677f8e6c9e74
k <k@klabz.org>
2018-12-14 09:26:36 +0100

    Added SwapHorizontalCircle icon

---
+++ src/mui/icon/SwapHorizontalCircle.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SwapHorizontalCircle')
+extern class SwapHorizontalCircle extends ReactComponentOfProps<IconProps> {}
