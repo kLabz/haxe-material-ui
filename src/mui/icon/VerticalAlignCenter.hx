0a791238ef7e5966e3e26c1f0ead7e2d03d26dcd
k <k@klabz.org>
2018-12-14 09:26:38 +0100

    Added VerticalAlignCenter icon

---
+++ src/mui/icon/VerticalAlignCenter.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'VerticalAlignCenter')
+extern class VerticalAlignCenter extends ReactComponentOfProps<IconProps> {}
