47b09bd886d32a0195f633bbd7de20f844e0dccf
k <k@klabz.org>
2018-12-14 09:26:30 +0100

    Added NotificationsNone icon

---
+++ src/mui/icon/NotificationsNone.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'NotificationsNone')
+extern class NotificationsNone extends ReactComponentOfProps<IconProps> {}
