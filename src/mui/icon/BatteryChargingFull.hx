42fd4cb8796e21e975b991858148a12a11e79ed7
k <k@klabz.org>
2018-12-14 09:26:19 +0100

    Added BatteryChargingFull icon

---
+++ src/mui/icon/BatteryChargingFull.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'BatteryChargingFull')
+extern class BatteryChargingFull extends ReactComponentOfProps<IconProps> {}
