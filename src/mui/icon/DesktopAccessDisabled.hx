f2ced8537f5c0ac7abcb68c9b1791a084840786e
k <k@klabz.org>
2018-12-14 09:26:22 +0100

    Added DesktopAccessDisabled icon

---
+++ src/mui/icon/DesktopAccessDisabled.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'DesktopAccessDisabled')
+extern class DesktopAccessDisabled extends ReactComponentOfProps<IconProps> {}
