a5ad7a6254ad6f1fb9335a81bbfea057539684cc
k <k@klabz.org>
2018-12-14 09:26:30 +0100

    Added NotificationsPaused icon

---
+++ src/mui/icon/NotificationsPaused.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'NotificationsPaused')
+extern class NotificationsPaused extends ReactComponentOfProps<IconProps> {}
