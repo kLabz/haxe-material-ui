8d198ff5e0acd64c368d0e5530f47cb1eafad3ba
k <k@klabz.org>
2018-12-14 09:26:19 +0100

    Added BluetoothConnected icon

---
+++ src/mui/icon/BluetoothConnected.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'BluetoothConnected')
+extern class BluetoothConnected extends ReactComponentOfProps<IconProps> {}
