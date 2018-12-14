750bee8d47ee92ddd58bef94038739bc0d90da95
k <k@klabz.org>
2018-12-14 09:26:36 +0100

    Added SubdirectoryArrowLeft icon

---
+++ src/mui/icon/SubdirectoryArrowLeft.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SubdirectoryArrowLeft')
+extern class SubdirectoryArrowLeft extends ReactComponentOfProps<IconProps> {}
