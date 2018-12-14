ab513e80028bbe79992098e9f14c0e1da9abdacd
k <k@klabz.org>
2018-12-14 09:26:27 +0100

    Added InsertChartOutlined icon

---
+++ src/mui/icon/InsertChartOutlined.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'InsertChartOutlined')
+extern class InsertChartOutlined extends ReactComponentOfProps<IconProps> {}
