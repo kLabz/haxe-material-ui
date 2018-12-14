606cc987f521491344ab954e22075c54331cea32
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SentimentVerySatisfied icon

---
+++ src/mui/icon/SentimentVerySatisfied.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SentimentVerySatisfied')
+extern class SentimentVerySatisfied extends ReactComponentOfProps<IconProps> {}
