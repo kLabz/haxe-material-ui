f95ae7da553ec6df200da33e164cbb69b875a653
k <k@klabz.org>
2018-12-14 09:26:19 +0100

    Added BluetoothSearching icon

---
+++ src/mui/icon/BluetoothSearching.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'BluetoothSearching')
+extern class BluetoothSearching extends ReactComponentOfProps<IconProps> {}
