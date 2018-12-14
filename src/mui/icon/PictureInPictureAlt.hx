e928676e24eb66583d43e9dc9e15f793ee2e0381
k <k@klabz.org>
2018-12-14 09:26:32 +0100

    Added PictureInPictureAlt icon

---
+++ src/mui/icon/PictureInPictureAlt.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'PictureInPictureAlt')
+extern class PictureInPictureAlt extends ReactComponentOfProps<IconProps> {}
