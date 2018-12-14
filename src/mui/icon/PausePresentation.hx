069653a72e813e3dc85302eacb0d9a033af5f1bd
k <k@klabz.org>
2018-12-14 09:26:31 +0100

    Added PausePresentation icon

---
+++ src/mui/icon/PausePresentation.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'PausePresentation')
+extern class PausePresentation extends ReactComponentOfProps<IconProps> {}
