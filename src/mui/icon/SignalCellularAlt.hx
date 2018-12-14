0b538bc87f2c88f917969cdea25389c4dfb7c5d5
k <k@klabz.org>
2018-12-14 09:26:35 +0100

    Added SignalCellularAlt icon

---
+++ src/mui/icon/SignalCellularAlt.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SignalCellularAlt')
+extern class SignalCellularAlt extends ReactComponentOfProps<IconProps> {}
