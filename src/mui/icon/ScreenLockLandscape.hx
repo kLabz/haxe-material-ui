f1aaf50b78d204bc6a00257da1a5d646062875e7
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added ScreenLockLandscape icon

---
+++ src/mui/icon/ScreenLockLandscape.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'ScreenLockLandscape')
+extern class ScreenLockLandscape extends ReactComponentOfProps<IconProps> {}
