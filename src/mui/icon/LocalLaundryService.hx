52062dbccd2b2f6a0b5d7504b4aa6769c75f2666
k <k@klabz.org>
2018-12-14 09:26:28 +0100

    Added LocalLaundryService icon

---
+++ src/mui/icon/LocalLaundryService.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'LocalLaundryService')
+extern class LocalLaundryService extends ReactComponentOfProps<IconProps> {}
