ca2b1fa63935cd49ad4ea8b8db4a7231cbf20c8e
k <k@klabz.org>
2018-12-14 09:26:19 +0100

    Added BatteryCharging50 icon

---
+++ src/mui/icon/BatteryCharging50.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'BatteryCharging50')
+extern class BatteryCharging50 extends ReactComponentOfProps<IconProps> {}
