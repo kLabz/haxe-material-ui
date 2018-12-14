f5254142a7a27e75289ebb56da45f191673cd281
k <k@klabz.org>
2018-12-14 09:26:36 +0100

    Added SwapVerticalCircle icon

---
+++ src/mui/icon/SwapVerticalCircle.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SwapVerticalCircle')
+extern class SwapVerticalCircle extends ReactComponentOfProps<IconProps> {}
