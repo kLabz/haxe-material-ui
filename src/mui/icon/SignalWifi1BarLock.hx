f5fcdffb1dc9a339f8899e707f73d30e97aa5301
k <k@klabz.org>
2018-12-14 09:26:35 +0100

    Added SignalWifi1BarLock icon

---
+++ src/mui/icon/SignalWifi1BarLock.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SignalWifi1BarLock')
+extern class SignalWifi1BarLock extends ReactComponentOfProps<IconProps> {}
