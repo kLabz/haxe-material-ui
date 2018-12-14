0a3013b2876adc50b1be242615eddb13ba21836c
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SettingsBluetooth icon

---
+++ src/mui/icon/SettingsBluetooth.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SettingsBluetooth')
+extern class SettingsBluetooth extends ReactComponentOfProps<IconProps> {}
