295ca1a083c0421655eea970347c33ebded28964
k <k@klabz.org>
2018-12-14 09:26:35 +0100

    Added SignalCellularNull icon

---
+++ src/mui/icon/SignalCellularNull.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SignalCellularNull')
+extern class SignalCellularNull extends ReactComponentOfProps<IconProps> {}
