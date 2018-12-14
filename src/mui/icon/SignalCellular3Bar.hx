cea103b464fcc0143f287af78062c43c6a340975
k <k@klabz.org>
2018-12-14 09:26:35 +0100

    Added SignalCellular3Bar icon

---
+++ src/mui/icon/SignalCellular3Bar.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SignalCellular3Bar')
+extern class SignalCellular3Bar extends ReactComponentOfProps<IconProps> {}
