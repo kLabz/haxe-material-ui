5fe56e8373e1a9edf802b04853ed6d7318a7e6a2
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SettingsInputHdmi icon

---
+++ src/mui/icon/SettingsInputHdmi.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SettingsInputHdmi')
+extern class SettingsInputHdmi extends ReactComponentOfProps<IconProps> {}
