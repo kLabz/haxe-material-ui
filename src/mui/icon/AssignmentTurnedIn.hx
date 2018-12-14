fbac9e7549d2747c5ee6b02390128ee9cffd6d1e
k <k@klabz.org>
2018-12-14 09:26:19 +0100

    Added AssignmentTurnedIn icon

---
+++ src/mui/icon/AssignmentTurnedIn.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'AssignmentTurnedIn')
+extern class AssignmentTurnedIn extends ReactComponentOfProps<IconProps> {}
