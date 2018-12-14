12b56b59740be3ffdaf597ccc2b3966c2d23a39a
k <k@klabz.org>
2018-12-14 09:26:25 +0100

    Added FormatTextdirectionRToL icon

---
+++ src/mui/icon/FormatTextdirectionRToL.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'FormatTextdirectionRToL')
+extern class FormatTextdirectionRToL extends ReactComponentOfProps<IconProps> {}
