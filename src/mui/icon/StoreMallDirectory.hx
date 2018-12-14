6b9be28da1bc949277ca6c41b4d845dcb28b4b5f
k <k@klabz.org>
2018-12-14 09:26:36 +0100

    Added StoreMallDirectory icon

---
+++ src/mui/icon/StoreMallDirectory.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'StoreMallDirectory')
+extern class StoreMallDirectory extends ReactComponentOfProps<IconProps> {}
