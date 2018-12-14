9c53aad1b437d00aa88ede189015e4458805c8e7
k <k@klabz.org>
2018-12-14 09:26:18 +0100

    Added AirplanemodeActive icon

---
+++ src/mui/icon/AirplanemodeActive.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'AirplanemodeActive')
+extern class AirplanemodeActive extends ReactComponentOfProps<IconProps> {}
