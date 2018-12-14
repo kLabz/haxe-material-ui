b525e732bb63c7a23709a8d7c17e9723cbe49f51
k <k@klabz.org>
2018-12-14 09:26:21 +0100

    Added ChatBubbleOutline icon

---
+++ src/mui/icon/ChatBubbleOutline.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'ChatBubbleOutline')
+extern class ChatBubbleOutline extends ReactComponentOfProps<IconProps> {}
