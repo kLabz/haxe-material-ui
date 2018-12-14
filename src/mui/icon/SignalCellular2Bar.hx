89313968fb511f658494c2c2e0cb543c5fa0f908
k <k@klabz.org>
2018-12-14 09:26:35 +0100

    Added SignalCellular2Bar icon

---
+++ src/mui/icon/SignalCellular2Bar.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SignalCellular2Bar')
+extern class SignalCellular2Bar extends ReactComponentOfProps<IconProps> {}
