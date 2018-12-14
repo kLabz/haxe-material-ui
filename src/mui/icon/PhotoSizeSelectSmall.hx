290ecc00011c707af953e6f2e7c10de7a4a2a8e2
k <k@klabz.org>
2018-12-14 09:26:32 +0100

    Added PhotoSizeSelectSmall icon

---
+++ src/mui/icon/PhotoSizeSelectSmall.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'PhotoSizeSelectSmall')
+extern class PhotoSizeSelectSmall extends ReactComponentOfProps<IconProps> {}
