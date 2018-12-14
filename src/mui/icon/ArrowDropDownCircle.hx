e11468b98abd932977b029d0139e073f0dc4ab1a
k <k@klabz.org>
2018-12-14 09:26:18 +0100

    Added ArrowDropDownCircle icon

---
+++ src/mui/icon/ArrowDropDownCircle.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'ArrowDropDownCircle')
+extern class ArrowDropDownCircle extends ReactComponentOfProps<IconProps> {}
