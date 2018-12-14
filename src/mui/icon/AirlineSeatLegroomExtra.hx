64ce494df227b6338a14ddf345af214015eda2aa
k <k@klabz.org>
2018-12-14 09:26:18 +0100

    Added AirlineSeatLegroomExtra icon

---
+++ src/mui/icon/AirlineSeatLegroomExtra.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'AirlineSeatLegroomExtra')
+extern class AirlineSeatLegroomExtra extends ReactComponentOfProps<IconProps> {}
