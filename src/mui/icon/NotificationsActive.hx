de424f963fa6e2af2dca32e6fc75c88d1bccba94
k <k@klabz.org>
2018-12-14 09:26:30 +0100

    Added NotificationsActive icon

---
+++ src/mui/icon/NotificationsActive.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'NotificationsActive')
+extern class NotificationsActive extends ReactComponentOfProps<IconProps> {}
