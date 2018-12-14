e26f1b0601c01ce5cf57b276e6292d44776cabbd
k <k@klabz.org>
2018-12-14 09:26:22 +0100

    Added DirectionsRailway icon

---
+++ src/mui/icon/DirectionsRailway.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'DirectionsRailway')
+extern class DirectionsRailway extends ReactComponentOfProps<IconProps> {}
