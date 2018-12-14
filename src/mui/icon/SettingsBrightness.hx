3c809ea8225cd9343299626210f659aa5f99eb13
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SettingsBrightness icon

---
+++ src/mui/icon/SettingsBrightness.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SettingsBrightness')
+extern class SettingsBrightness extends ReactComponentOfProps<IconProps> {}
