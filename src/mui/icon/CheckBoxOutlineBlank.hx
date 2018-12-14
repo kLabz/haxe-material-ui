62cc979baac34d2e2e4efeb79ed6bba49c694c01
k <k@klabz.org>
2018-12-14 09:26:21 +0100

    Added CheckBoxOutlineBlank icon

---
+++ src/mui/icon/CheckBoxOutlineBlank.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'CheckBoxOutlineBlank')
+extern class CheckBoxOutlineBlank extends ReactComponentOfProps<IconProps> {}
