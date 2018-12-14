195ec36c257045995c4eed6d722da171af278097
k <k@klabz.org>
2018-12-14 09:26:36 +0100

    Added StayPrimaryPortrait icon

---
+++ src/mui/icon/StayPrimaryPortrait.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'StayPrimaryPortrait')
+extern class StayPrimaryPortrait extends ReactComponentOfProps<IconProps> {}
