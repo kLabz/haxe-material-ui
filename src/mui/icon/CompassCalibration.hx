c9cd078f1d903eaecdd300b2588072ef484fc8de
k <k@klabz.org>
2018-12-14 09:26:21 +0100

    Added CompassCalibration icon

---
+++ src/mui/icon/CompassCalibration.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'CompassCalibration')
+extern class CompassCalibration extends ReactComponentOfProps<IconProps> {}
