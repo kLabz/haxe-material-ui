2f356d8b2ebe2b7f9396dcfccc4d34fe8a32f573
k <k@klabz.org>
2018-12-14 09:26:20 +0100

    Added CallMissedOutgoing icon

---
+++ src/mui/icon/CallMissedOutgoing.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'CallMissedOutgoing')
+extern class CallMissedOutgoing extends ReactComponentOfProps<IconProps> {}
