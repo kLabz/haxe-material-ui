1218474864351c9bbac157b33da6e295ae599ab7
k <k@klabz.org>
2018-12-14 09:26:18 +0100

    Added AirlineSeatFlatAngled icon

---
+++ src/mui/icon/AirlineSeatFlatAngled.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'AirlineSeatFlatAngled')
+extern class AirlineSeatFlatAngled extends ReactComponentOfProps<IconProps> {}
