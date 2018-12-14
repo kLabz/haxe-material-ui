e752f5613aeca54a8d289936b65f8debfd51579f
k <k@klabz.org>
2018-12-14 09:26:30 +0100

    Added NotListedLocation icon

---
+++ src/mui/icon/NotListedLocation.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'NotListedLocation')
+extern class NotListedLocation extends ReactComponentOfProps<IconProps> {}
