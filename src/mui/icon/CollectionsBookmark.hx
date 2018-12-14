6822fd7ba1addd4e1ef23d798f6226718912de2f
k <k@klabz.org>
2018-12-14 09:26:21 +0100

    Added CollectionsBookmark icon

---
+++ src/mui/icon/CollectionsBookmark.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'CollectionsBookmark')
+extern class CollectionsBookmark extends ReactComponentOfProps<IconProps> {}
