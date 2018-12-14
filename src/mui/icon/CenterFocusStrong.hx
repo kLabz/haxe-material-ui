f31c82bef762a32fca2a2647cf2c4ab1a3a18c5f
k <k@klabz.org>
2018-12-14 09:26:21 +0100

    Added CenterFocusStrong icon

---
+++ src/mui/icon/CenterFocusStrong.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'CenterFocusStrong')
+extern class CenterFocusStrong extends ReactComponentOfProps<IconProps> {}
