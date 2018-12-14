53028383746587faf1e4bf01632fd83b5bc4d44b
k <k@klabz.org>
2018-12-14 09:26:19 +0100

    Added BluetoothDisabled icon

---
+++ src/mui/icon/BluetoothDisabled.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'BluetoothDisabled')
+extern class BluetoothDisabled extends ReactComponentOfProps<IconProps> {}
