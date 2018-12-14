6661be7a12f9602cb9e489b10314ca3be8feb8cf
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added SettingsInputAntenna icon

---
+++ src/mui/icon/SettingsInputAntenna.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'SettingsInputAntenna')
+extern class SettingsInputAntenna extends ReactComponentOfProps<IconProps> {}
