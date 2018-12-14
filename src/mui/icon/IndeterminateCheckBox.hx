f920f84b54f9ae1d3010cdc0956376102bcf2717
k <k@klabz.org>
2018-12-14 09:26:27 +0100

    Added IndeterminateCheckBox icon

---
+++ src/mui/icon/IndeterminateCheckBox.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'IndeterminateCheckBox')
+extern class IndeterminateCheckBox extends ReactComponentOfProps<IconProps> {}
