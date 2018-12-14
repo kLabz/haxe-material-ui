b7447750698cc7b88b233a853d6f5697eda68147
k <k@klabz.org>
2018-12-14 09:26:25 +0100

    Added FormatLineSpacing icon

---
+++ src/mui/icon/FormatLineSpacing.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'FormatLineSpacing')
+extern class FormatLineSpacing extends ReactComponentOfProps<IconProps> {}
