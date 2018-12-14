9832696850a35ec6ac1e928570534f0686d3f544
k <k@klabz.org>
2018-12-14 09:26:18 +0100

    Added AirlineSeatIndividualSuite icon

---
+++ src/mui/icon/AirlineSeatIndividualSuite.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'AirlineSeatIndividualSuite')
+extern class AirlineSeatIndividualSuite extends ReactComponentOfProps<IconProps> {}
