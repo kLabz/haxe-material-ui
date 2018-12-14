ec920fd53766f4f747de53e8a667c6bd538dabe0
k <k@klabz.org>
2018-12-14 09:26:34 +0100

    Added ScreenLockPortrait icon

---
+++ src/mui/icon/ScreenLockPortrait.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'ScreenLockPortrait')
+extern class ScreenLockPortrait extends ReactComponentOfProps<IconProps> {}
