eeaeb7531dfdace16f47e1734c4f50d38ec47980
k <k@klabz.org>
2018-12-14 09:26:30 +0100

    Added NotificationImportant icon

---
+++ src/mui/icon/NotificationImportant.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'NotificationImportant')
+extern class NotificationImportant extends ReactComponentOfProps<IconProps> {}
