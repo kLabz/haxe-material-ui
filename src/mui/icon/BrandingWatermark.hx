2d2394104f2f5130a7ba622631dc86ace7204e7d
k <k@klabz.org>
2018-12-14 09:26:20 +0100

    Added BrandingWatermark icon

---
+++ src/mui/icon/BrandingWatermark.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'BrandingWatermark')
+extern class BrandingWatermark extends ReactComponentOfProps<IconProps> {}
