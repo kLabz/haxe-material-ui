04de832862c72e9b84e92f4824d22c9116149e16
k <k@klabz.org>
2018-12-14 09:26:24 +0100

    Added FilterCenterFocus icon

---
+++ src/mui/icon/FilterCenterFocus.hx
@@ -0,0 +1,6 @@
+package mui.icon;
+
+import react.ReactComponent;
+
+@:jsRequire('@material-ui/icons', 'FilterCenterFocus')
+extern class FilterCenterFocus extends ReactComponentOfProps<IconProps> {}
