e4b1e5a399b06c8e8870e8d491d3437a8778c41d
k <k@klabz.org>
2018-12-17 21:19:32 +0100

    IconProps: added missing component prop

--- src/mui/icon/IconProps.hx
+++ src/mui/icon/IconProps.hx
@@ -3,10 +3,13 @@ package mui.icon;
 import js.html.HtmlElement;
 
 import css.Properties;
+import react.ReactType;
 import react.types.DOMAttributes;
 
 typedef IconProps = {
 	> DOMAttributes<HtmlElement>,
+
+	@:optional var component:ReactType;
 	@:optional var color:IconColor;
 	@:optional var fontSize:FontSize;
 	@:optional var className:String;
