585c12aeaa29469832dfd290418ea9fd80461eaf
k <k@klabz.org>
2019-01-26 20:44:35 +0100

    ExpansionPanel: add Rounded to ExpansionPanelClassKey

--- src/mui/core/exppanel/ExpansionPanelClassKey.hx
+++ src/mui/core/exppanel/ExpansionPanelClassKey.hx
@@ -2,6 +2,7 @@ package mui.core.exppanel;
 
 @:enum abstract ExpansionPanelClassKey(String) to String {
 	var Root = "root";
+	var Rounded = "rounded";
 	var Expanded = "expanded";
 	var Disabled = "disabled";
 }
