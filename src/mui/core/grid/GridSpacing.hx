a7dd182bcf4690dd3e6d759dbf32e131621e4f44
Rudy G <k@klabz.org>
2019-10-02 15:28:04 +0200

    Grid: update spacing to new values

--- src/mui/core/grid/GridSpacing.hx
+++ src/mui/core/grid/GridSpacing.hx
@@ -1,10 +1,15 @@
 package mui.core.grid;
 
-@:enum abstract GridSpacing(Int) to Int from Int {
+@:enum abstract GridSpacing(Int) to Int {
 	var Spacing_0 = 0;
+	var Spacing_1 = 1;
+	var Spacing_2 = 2;
+	var Spacing_3 = 3;
+	var Spacing_4 = 4;
+	var Spacing_5 = 5;
+	var Spacing_6 = 6;
+	var Spacing_7 = 7;
 	var Spacing_8 = 8;
-	var Spacing_16 = 16;
-	var Spacing_24 = 24;
-	var Spacing_32 = 32;
-	var Spacing_40 = 40;
+	var Spacing_9 = 9;
+	var Spacing_10 = 10;
 }
