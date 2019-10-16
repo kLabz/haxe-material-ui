6d835310cbdcc7d2eb161e19ee5fd60e5d4828e1
Rudy G <k@klabz.org>
2019-10-16 12:11:45 +0200

    Fix props updates

--- src/mui/core/MenuItem.hx
+++ src/mui/core/MenuItem.hx
@@ -6,6 +6,7 @@ import mui.core.menu.MenuItemClassKey;
 typedef MenuItemProps = ForcedOverride<ListItemProps, {
 	@:optional var classes:Record<MenuItemClassKey>;
 	@:optional var component:ReactType;
+	@:optional var value:Any;
 }>;
 
 @:jsRequire('@material-ui/core', 'MenuItem')
