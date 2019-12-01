bbcdf53f5f9ae1c20bb526cb9b709d6976426d1f
k <k@klabz.org>
2019-12-01 11:21:11 +0100

    Update Avatar to 4.7.0

--- src/mui/core/avatar/AvatarClassKey.hx
+++ src/mui/core/avatar/AvatarClassKey.hx
@@ -3,5 +3,8 @@ package mui.core.avatar;
 @:enum abstract AvatarClassKey(String) to String {
 	var Root = "root";
 	var ColorDefault = "colorDefault";
+	var Circle = "circle";
+	var Rounded = "rounded";
+	var Square = "square";
 	var Img = "img";
 }
---
+++ src/mui/core/avatar/AvatarVariant.hx
@@ -0,0 +1,7 @@
+package mui.core.avatar;
+
+@:enum abstract AvatarVariant(String) to String {
+	var Circle = "circle";
+	var Rounded = "rounded";
+	var Square = "square";
+}
