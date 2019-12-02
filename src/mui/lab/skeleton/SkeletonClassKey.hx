6ad6280f377ccfaa9e9f23e59006cdbc462cfea9
k <k@klabz.org>
2019-12-02 07:20:46 +0100

    [Lab] Add Skeleton component

---
+++ src/mui/lab/skeleton/SkeletonClassKey.hx
@@ -0,0 +1,9 @@
+package mui.lab.skeleton;
+
+@:enum abstract SkeletonClassKey(String) to String {
+	var Root = "root";
+	var Text = "text";
+	var Rect = "rect";
+	var Circle = "circle";
+	var Animate = "animate";
+}
---
+++ src/mui/lab/skeleton/SkeletonVariant.hx
@@ -0,0 +1,7 @@
+package mui.lab.skeleton;
+
+@:enum abstract SkeletonVariant(String) to String {
+	var Text = "text";
+	var Rect = "rect";
+	var Circle = "circle";
+}
