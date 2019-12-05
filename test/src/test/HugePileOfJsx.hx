184c81da06a50502cb84e3760bef1a48367c3088
k <k@klabz.org>
2019-12-05 21:04:23 +0100

    Update tests to test all core + lab components

--- test/src/test/HugePileOfJsx.hx
+++ test/src/test/HugePileOfJsx.hx
@@ -1,10 +1,13 @@
 package test;
 
 import buddy.SingleSuite;
+import jsdom.Jsdom;
 import react.React;
 import react.ReactMacro.jsx;
 
-import mui.core.*;
+import mui.lab.*;
+import mui.core.*; // After lab to overwrite proxies
+import mui.core.styles.MuiStylesProvider;
 import mui.core.styles.MuiTheme;
 import mui.core.styles.MuiThemeProvider;
 
@@ -12,49 +15,66 @@ using buddy.Should;
 
 class HugePileOfJsx extends SingleSuite {
 	public function new() {
+		JsdomSetup.init();
+
 		var defaultTheme = MuiTheme.createMuiTheme({});
+		var noop = (?_) -> {};
+		var elt = jsx(<div />);
 
 		describe("Material UI components", {
 			// TODO: split up and use more props
+			// TODO: add lab components: Autocomplete, Rating, TreeItem, TreeView
 			it("can be included in jsx", {
-				var ret = jsx(
+				var ret = jsx(<>
+					<MuiStylesProvider>
+						<>
+							<Skeleton />
+							<SpeedDial ariaLabel="" open />
+							<SpeedDialAction />
+							<SpeedDialIcon />
+							<ToggleButton children={null} value={null} />
+							<ToggleButtonGroup children={null} />
+						</>
+					</MuiStylesProvider>
+
 					<MuiThemeProvider theme={defaultTheme}>
 						<>
 							<CssBaseline />
-
 							<AppBar children={null} />
 							<Avatar />
 							<Backdrop open />
 							<Badge />
-							<BottomNavigationAction />
 							<BottomNavigation children={null} />
+							<BottomNavigationAction />
 							<Breadcrumbs children={null} />
+							<Button children={null} />
 							<ButtonBase />
 							<ButtonGroup children={null} />
-							<Button children={null} />
+							<Card />
 							<CardActionArea />
 							<CardActions />
 							<CardContent />
 							<CardHeader />
-							<Card />
 							<CardMedia />
 							<Checkbox />
 							<Chip />
 							<CircularProgress />
+							<ClickAwayListener children={elt} onClickAway={noop} />
 							<Collapse />
 							<Container children={null} />
+							<Dialog children={null} open />
 							<DialogActions />
 							<DialogContent />
 							<DialogContentText />
-							<Dialog children={null} open />
 							<DialogTitle children={null} />
 							<Divider />
 							<Drawer />
+							<ExpansionPanel children={null} />
 							<ExpansionPanelActions />
 							<ExpansionPanelDetails />
-							<ExpansionPanel children={null} />
 							<ExpansionPanelSummary />
 							<Fab children={null} />
+							<Fade />
 							<FilledInput />
 							<FormControl />
 							<FormControlLabel />
@@ -63,18 +83,20 @@ class HugePileOfJsx extends SingleSuite {
 							<FormLabel />
 							<Grid />
 							<GridList children={null} />
-							<GridListTileBar />
 							<GridListTile />
+							<GridListTileBar />
+							<Grow />
+							<Hidden />
 							<IconButton />
+							<Input />
 							<InputAdornment children={null} />
 							<InputBase />
-							<Input />
 							<InputLabel />
 							<LinearProgress />
 							<Link children={null} />
 							<List />
-							<ListItemAvatar children={null} />
 							<ListItem />
+							<ListItemAvatar children={null} />
 							<ListItemIcon children={null} />
 							<ListItemSecondaryAction />
 							<ListItemText />
@@ -85,32 +107,36 @@ class HugePileOfJsx extends SingleSuite {
 							<MobileStepper steps={1} />
 							<Modal children={<Grid />} open />
 							<NativeSelect />
+							<NoSsr children={elt} />
 							<OutlinedInput labelWidth={0} />
 							<Paper />
 							<Popover open />
-							<Popper children={<Grid />} open />
+							<Popper children={<Grid />} open={false} />
 							<Portal />
-							<RadioGroup />
 							<Radio />
+							<RadioGroup />
 							<RootRef children={<Grid />} rootRef={React.createRef()} />
+							<Select />
+							<Slide />
 							<Slider />
-							<SnackbarContent />
 							<Snackbar />
+							<SnackbarContent />
+							<Step />
 							<StepButton />
 							<StepConnector />
 							<StepContent />
-							<Step />
 							<StepIcon icon={null} />
 							<StepLabel />
 							<Stepper children={null} />
 							<SvgIcon children={null} />
+							<SwipeableDrawer onClose={noop} onOpen={noop} open />
 							<Switch />
 							<Tab />
+							<Table children={null} />
 							<TableBody children={null} />
 							<TableCell />
 							<TableFooter children={null} />
 							<TableHead children={null} />
-							<Table children={null} />
 							<TablePagination count={1} page={0} onChangePage={null} rowsPerPage={10} />
 							<TableRow children={null} />
 							<TableSortLabel />
@@ -124,7 +150,7 @@ class HugePileOfJsx extends SingleSuite {
 
 						</>
 					</MuiThemeProvider>
-				);
+				</>);
 
 				ret.should.not.be(null);
 			});
