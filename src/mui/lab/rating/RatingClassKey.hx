38d25ec9ab64691b0d16b9d1ae74f1e91c2f56b5
k <k@klabz.org>
2019-12-05 22:07:12 +0100

    Lab: add Rating component

---
+++ src/mui/lab/rating/RatingClassKey.hx
@@ -0,0 +1,20 @@
+package mui.lab.rating;
+
+@:enum abstract RatingClassKey(String) to String {
+	var Root = "root";
+	var SizeSmall = "sizeSmall";
+	var SizeLarge = "sizeLarge";
+	var ReadOnly = "readOnly";
+	var Disabled = "disabled";
+	var FocusVisible = "focusVisible";
+	var VisuallyHidden = "visuallyHidden";
+	var Pristine = "pristine";
+	var Label = "label";
+	var Icon = "icon";
+	var IconEmpty = "iconEmpty";
+	var IconFilled = "iconFilled";
+	var IconHover = "iconHover";
+	var IconFocus = "iconFocus";
+	var IconActive = "iconActive";
+	var Decimal = "decimal";
+}
---
+++ src/mui/lab/rating/RatingSize.hx
@@ -0,0 +1,7 @@
+package mui.lab.rating;
+
+@:enum abstract RatingSize(String) {
+	var Small = "small";
+	var Medium = "medium";
+	var Large = "large";
+}
