9ea04fa62f38c0c3383a4a7163ca86a752cb25e5
k <k@klabz.org>
2019-12-07 14:41:55 +0100

    Avatar: add fallback classkey

--- src/mui/core/avatar/AvatarClassKey.hx
+++ src/mui/core/avatar/AvatarClassKey.hx
@@ -3,6 +3,7 @@ package mui.core.avatar;
 @:enum abstract AvatarClassKey(String) to String {
 	var Root = "root";
 	var ColorDefault = "colorDefault";
+	var Fallback = "fallback";
 	var Circle = "circle";
 	var Rounded = "rounded";
 	var Square = "square";
