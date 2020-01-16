1d1672a8aa7d243812013db1363665c710ecabca
Rudy G <k@klabz.org>
2020-01-16 11:30:11 +0100

    Remove unneeded ForcedOverride

--- src/mui/core/Slide.hx
+++ src/mui/core/Slide.hx
@@ -3,11 +3,12 @@ package mui.core;
 import react.transition.Transition;
 import mui.core.common.Direction;
 
-typedef SlideProps = ForcedOverride<TransitionProps<Any>, {
+typedef SlideProps = {
+	> TransitionProps<Any>,
 	> StandardDOMAttributes,
 
 	@:optional var direction:Direction;
-}>;
+}
 
 @:acceptsMoreProps('react.transition.Transition')
 @:jsRequire('@material-ui/core', 'Slide')
