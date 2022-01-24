a55c6f170d1d1311689448f07a08162d6ab2997f
Jarrio <Jarrio@users.noreply.github.com>
2022-01-24 00:44:46 +0000

    Added alpha() extern to the API

--- src/mui/core/styles/Styles.hx
+++ src/mui/core/styles/Styles.hx
@@ -14,6 +14,7 @@ import haxe.macro.Expr;
 
 @:jsRequire('@material-ui/core/styles')
 extern class Styles {
+	extern static function alpha(color:ColorString, opacity:Float):ColorString;
 	public static inline macro function jss(styles:Expr) {
 		return switch (styles.expr) {
 			case EObjectDecl(fields):
